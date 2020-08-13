310b7c1c131a6ca5241e17e64f00c67c5d213561
koji-torishima <torikou3685@gmail.com>
2020-08-14 06:58:48 +0900

    画面遷移できるようにする

--- src/components/pages/Loading/index.tsx
+++ src/components/pages/Loading/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import * as UiContext from '../../../contexts/ui';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,12 +9,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 });
+
+function ChangeStateButton(props: {state: UiContext.Status}) {
+  const {setApplicationState} = React.useContext(UiContext.Context);
+  const {state} = props;
+
+  return (
+    <TouchableOpacity onPress={() => setApplicationState(state)}>
+      <Text style={styles.text}>Change state to {state}</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function Loading() {
   return (
     <View style={styles.container}>
       <Text>Loading</Text>
+    </View>
+  );
+}
+
+export default function () {
+  return (
+    <View style={styles.container}>
+      <ChangeStateButton state={UiContext.Status.AUTHORIZED} />
+      <ChangeStateButton state={UiContext.Status.UN_AUTHORIZED} />
+      <ChangeStateButton state={UiContext.Status.FIRST_OPEN} />
     </View>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainRoutes from './Main';
+
 export default function LoggingRoutes() {
   return (
     <NavigationContainer>
