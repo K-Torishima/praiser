c563446602914099c61e2ee0bfcd08412814ab73
koji-torishima <torikou3685@gmail.com>
2020-08-14 03:39:47 +0900

    ディレクトリ追加

---
+++ src/constants/path.ts
@@ -0,0 +1,16 @@
+// 画面パスを設定する画面
+// 最初に画面名を定数としておいたファイルを作成します
+export const INITIAL = 'INITIAL';
+export const HOME = 'HOME';
+export const DETAIL = 'DETAIL';
+export const SIGN_UP = 'SIGN_IN';
+export const USER_INFO = 'USER_INFO';
+export const STATISTICS = 'STATISTICS';
+export const LOADING = 'LOADING';
+export const CHOOSE_LOGIN = 'CHOOSE_LOGIN';
+export const INPUT = 'INPUT';
+
+//  定数化する理由
+// 変更を強くし、メンテナンスができるコードにする
+// 打ち間違えを防ぐ
+// ハードコーディングさせない
