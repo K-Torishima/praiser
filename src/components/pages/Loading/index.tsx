0dca73ebc56440e297ddb1741612fdb4b4cab37c
koji-torishima <torikou3685@gmail.com>
2020-08-16 14:22:17 +0900

    画面の色を帰る。

--- src/components/pages/Loading/index.tsx
+++ src/components/pages/Loading/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import * as UiContext from '../../../contexts/ui';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {COLOR} from '../../../constants/theme'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: COLOR.MAIN,
   },
   text: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: COLOR.WHITE,
   },
 });
 
@@ -27,14 +29,6 @@ function ChangeStateButton(props: {state: UiContext.Status}) {
 }
 
 export default function Loading() {
-  return (
-    <View style={styles.container}>
-      <Text>Loading</Text>
-    </View>
-  );
-}
-
-export default function () {
   return (
     <View style={styles.container}>
       <ChangeStateButton state={UiContext.Status.AUTHORIZED} />
